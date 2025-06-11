1640c5b76cea232d1c6598af302d9d21cc27b1b9
Jan Aries <sjanaries2005@gmail.com>
2025-06-11 14:51:21 +0800

    first commit

---
+++ workout-planner-api/src/main/java/com/workoutplanner/workout_planner_api/WorkoutPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.workoutplanner.workout_planner_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutPlannerApiApplication.class, args);
+	}
+
+}
---
+++ workout-planner-api/src/test/java/com/workoutplanner/workout_planner_api/WorkoutPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workoutplanner.workout_planner_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
