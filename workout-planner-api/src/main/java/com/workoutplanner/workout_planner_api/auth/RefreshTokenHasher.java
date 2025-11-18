d62869fb53546c6552410b7682a853af538c6e23
Jan Aries <sjanaries2005@gmail.com>
2025-11-18 20:50:46 +0800

    Add secret key for hash in RefreshTokenHasher

--- workout-planner-api/src/main/java/com/workoutplanner/workout_planner_api/auth/RefreshTokenHasher.java
+++ workout-planner-api/src/main/java/com/workoutplanner/workout_planner_api/auth/RefreshTokenHasher.java
@@ -1,30 +1,35 @@
 package com.workoutplanner.workout_planner_api.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 @Component
+@RequiredArgsConstructor
 public class RefreshTokenHasher {
 
+    @Value("${app.security.token-secret}")
+    private String secret;
 
-    public String hash(String refreshToken) {
-       try {
-           MessageDigest digest = MessageDigest.getInstance("SHA-256");
-           byte[] hashBytes = digest.digest(refreshToken.getBytes(StandardCharsets.UTF_8));
-           return Base64.getEncoder().encodeToString(hashBytes);
-       } catch (NoSuchAlgorithmException e) {
-           throw new RuntimeException("SHA-256 algorithm not available", e);
-       }
+    public String hash(String token) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest((token + secret).getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 algorithm not available", e);
+        }
     }
 
     public boolean matches(String rawToken, String hashedToken) {
-        String rawTokenHash = hash(rawToken);
-        return rawTokenHash.equals(hashedToken);
+        return hash(rawToken).equals(hashedToken);
     }
 }
